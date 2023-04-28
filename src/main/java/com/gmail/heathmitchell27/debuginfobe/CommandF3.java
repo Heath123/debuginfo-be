99cfffe84cfb876ee8aa3a37ac6d2d584329336c
Kyle Collins <65318126+Budderman18@users.noreply.github.com>
2023-04-28 12:42:49 -0400

    fix gamerule mistake

--- src/main/java/com/gmail/heathmitchell27/debuginfobe/CommandF3.java
+++ src/main/java/com/gmail/heathmitchell27/debuginfobe/CommandF3.java
@@ -5,7 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.GameRule<T>;
+import org.bukkit.GameRule;
 
 public class CommandF3 implements CommandExecutor {
 
@@ -30,7 +30,7 @@ public class CommandF3 implements CommandExecutor {
 	            try {
 	            	if(args.length==1)
 	            	{
-				if (player.getWorld().getGameRule(GameRule.REDUCED_DEBUG_INFO) == false)
+				if (player.getWorld().getGameRuleValue(GameRule.REDUCED_DEBUG_INFO) == false)
 				{
 	            			int number = Integer.parseInt(args[0]);
 	            			MyListener.particleLevel = number;
@@ -45,7 +45,7 @@ public class CommandF3 implements CommandExecutor {
 	            }
 	            if(args.length==0)
 		    {
-			if (player.getWorld().getGameRule(GameRule.REDUCED_DEBUG_INFO) == false)
+			if (player.getWorld().getGameRuleValue(GameRule.REDUCED_DEBUG_INFO) == false)
 			{
 	            		MyListener.showDebugScreenMap.put(player, !currentOption);
 			}
